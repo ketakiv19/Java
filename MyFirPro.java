b31288a9e072c6cd518c38f9241d52bd30ff75fb
ketakiv19 <167277351+ketakiv19@users.noreply.github.com>
2024-04-18 20:35:49 +0530

    Add files via upload

---
+++ MyFirPro.java
@@ -0,0 +1,23 @@
+package automation;
+
+public class MyFirPro {
+
+	public static void main(String[] args) {
+		
+		//local variable
+		
+				int a = 10 ;
+				
+				int b = 20 ;
+				
+				int c = a+b ;
+				
+				System.out.println(c);
+		
+		
+		
+		
+
+	}
+
+}
